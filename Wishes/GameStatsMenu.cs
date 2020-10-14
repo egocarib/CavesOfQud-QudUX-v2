3b2c9784f5c69e9cb8cd528fab77b6d144eb888c
egocarib <egocaribx@gmail.com>
2020-10-13 19:00:41 -0500

    simplify method that opens GameStatsMenu

--- Wishes/GameStatsMenu.cs
+++ Wishes/GameStatsMenu.cs
@@ -12,20 +12,14 @@ namespace QudUX.Wishes
         [WishCommand(Regex = @"(?:[Qq]ud[Uu][Xx] ?)?[Gg]amestats ?[Mm]enu")]
         public static void Wish(Match match = null)
         {
-            
-            var player = XRLCore.Core?.Game?.Player?.Body;
-            if (player != null)
+            try
             {
-                try
-                {
-                    ConsoleUtilities.SuppressScreenBufferImposters(true);
-                    var GameStatsMenu = new QudUX_GameStatsScreen();
-                    GameStatsMenu.Show(player);
-                }
-                finally
-                {
-                    ConsoleUtilities.SuppressScreenBufferImposters(false);
-                }
+                var GameStatsMenu = new QudUX_GameStatsScreen();
+                GameStatsMenu.Show(null);
+            }
+            catch (Exception ex)
+            {
+                Utilities.Logger.Log($"(Error) Encountered an exception while showing the Game Stats menu [{ex}]");
             }
         }
     }
